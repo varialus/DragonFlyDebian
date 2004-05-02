6ff6f96409b6c1db96fac3fd2db7d05d2535b66e
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2004-05-02 00:34:24 +0000

    move sysdeps/unix/bsd/bsd4.4/{,k}freebsd and fix associated files.

--- glibc-2.3/sysdeps/unix/bsd/bsd4.4/kfreebsd/alpha/getmntinfo.c
+++ glibc-2.3/sysdeps/unix/bsd/bsd4.4/kfreebsd/alpha/getmntinfo.c
@@ -2,7 +2,7 @@
    __fsblkcnt64_t == __fsblkcnt_t and __fsfilcnt64_t == __fsfilcnt_t.  */
 
 #define getmntinfo64 __no_getmntinfo64_decl
-#include <sysdeps/unix/bsd/bsd4.4/freebsd/getmntinfo.c>
+#include <sysdeps/unix/bsd/bsd4.4/kfreebsd/getmntinfo.c>
 #undef getmntinfo64
 
 weak_alias (__getmntinfo, getmntinfo64)
