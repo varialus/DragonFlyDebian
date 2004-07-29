d40c1132f1a9f27e25076e71ecc786f453a8217a
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2004-07-29 17:41:48 +0000

    Add krb5 and postgresql.

---
+++ patches/krb5_1.3.3.bash
@@ -0,0 +1,173 @@
+#!/bin/bash -e
+
+# Status: in BTS
+
+cat $0 | patch -p1
+(cd src && ACLOCAL=true ./util/reconf --force)
+exit 0
+
+diff -ur krb5-1.3.3.old/src/aclocal.m4 krb5-1.3.3/src/aclocal.m4
+--- krb5-1.3.3.old/src/aclocal.m4	2004-07-28 22:43:05.000000000 +0200
++++ krb5-1.3.3/src/aclocal.m4	2004-07-29 16:42:02.000000000 +0200
+@@ -1230,10 +1230,21 @@
+     AC_CHECK_LIB(socket, socket, LIBS="-lsocket -lnsl $LIBS", , -lnsl)))
+   KRB5_AC_ENABLE_DNS
+   if test "$enable_dns" = yes ; then
+-    AC_CHECK_FUNC(res_search, , AC_CHECK_LIB(resolv, res_search,
+-	LIBS="$LIBS -lresolv" ; RESOLV_LIB=-lresolv,
+-	AC_MSG_ERROR(Cannot find resolver support routine res_search in -lresolv.)
+-    ))
++	AC_MSG_CHECKING(for res_search)
++	AC_TRY_LINK_FUNC(res_search, AC_MSG_RESULT(yes),
++		AC_MSG_RESULT(no))
++	saved_LIBS="$LIBS"
++	LIBS="$LIBS -lresolv"
++	AC_MSG_CHECKING(for res_search in -lresolv)
++	AC_LINK_IFELSE([[
++#include <resolv.h>
++int main()
++{
++       res_search (0, 0, 0, 0, 0);
++       return 0;
++}]],
++		LIBS="$LIBS -lresolv"; RESOLV_LIB=-lresolv; AC_MSG_RESULT(yes),
++		LIBS="$saved_LIBS"; AC_MSG_RESULT(no))
+   fi
+   AC_SUBST(RESOLV_LIB)
+   ])
+diff -ur krb5-1.3.3.old/src/appl/gssftp/ftpd/ftpcmd.y krb5-1.3.3/src/appl/gssftp/ftpd/ftpcmd.y
+--- krb5-1.3.3.old/src/appl/gssftp/ftpd/ftpcmd.y	2002-10-23 17:00:23.000000000 +0200
++++ krb5-1.3.3/src/appl/gssftp/ftpd/ftpcmd.y	2004-07-29 16:42:02.000000000 +0200
+@@ -96,7 +96,7 @@
+ #endif
+ 
+ #ifndef NBBY
+-#ifdef linux
++#ifdef unix
+ #define NBBY 8
+ #endif
+ #ifdef __pyrsoft
+diff -ur krb5-1.3.3.old/src/appl/telnet/telnet/sys_bsd.c krb5-1.3.3/src/appl/telnet/telnet/sys_bsd.c
+--- krb5-1.3.3.old/src/appl/telnet/telnet/sys_bsd.c	2002-11-15 21:21:35.000000000 +0100
++++ krb5-1.3.3/src/appl/telnet/telnet/sys_bsd.c	2004-07-29 16:48:53.000000000 +0200
+@@ -52,6 +52,9 @@
+ #endif /* POSIX_SIGNALS */
+ #include <errno.h>
+ #include <arpa/telnet.h>
++#ifdef USE_TERMIO
++#include <termios.h>
++#endif
+ 
+ #include "ring.h"
+ 
+@@ -116,32 +119,22 @@
+ # define old_tc ottyb
+ 
+ #else	/* USE_TERMIO */
+-struct	termio old_tc = { 0 };
+-extern struct termio new_tc;
++struct	termios old_tc = { 0 };
++extern struct termios new_tc;
+ 
+ # ifndef	TCSANOW
+ #  ifdef TCSETS
+ #   define	TCSANOW		TCSETS
+ #   define	TCSADRAIN	TCSETSW
+-#   define	tcgetattr(f, t) ioctl(f, TCGETS, (char *)t)
+ #  else
+ #   ifdef TCSETA
+ #    define	TCSANOW		TCSETA
+ #    define	TCSADRAIN	TCSETAW
+-#    define	tcgetattr(f, t) ioctl(f, TCGETA, (char *)t)
+ #   else
+ #    define	TCSANOW		TIOCSETA
+ #    define	TCSADRAIN	TIOCSETAW
+-#    define	tcgetattr(f, t) ioctl(f, TIOCGETA, (char *)t)
+ #   endif
+ #  endif
+-#  define	tcsetattr(f, a, t) ioctl(f, a, (char *)t)
+-#  define	cfgetospeed(ptr)	((ptr)->c_cflag&CBAUD)
+-#  ifdef CIBAUD
+-#   define	cfgetispeed(ptr)	(((ptr)->c_cflag&CIBAUD) >> IBSHIFT)
+-#  else
+-#   define	cfgetispeed(ptr)	cfgetospeed(ptr)
+-#  endif
+ # endif /* TCSANOW */
+ # ifdef	sysV88
+ # define TIOCFLUSH TC_PX_DRAIN
+@@ -258,7 +251,9 @@
+     void
+ TerminalFlushOutput()
+ {
+-#ifdef	TIOCFLUSH
++#if defined(USE_TERMIO)
++    (void) tcflush (fileno(stdout), TCIOFLUSH);
++#elif defined(TIOCFLUSH)
+     (void) ioctl(fileno(stdout), TIOCFLUSH, (char *) 0);
+ #else
+     (void) ioctl(fileno(stdout), TCFLSH, (char *) 0);
+diff -ur krb5-1.3.3.old/src/appl/telnet/telnetd/configure.in krb5-1.3.3/src/appl/telnet/telnetd/configure.in
+--- krb5-1.3.3.old/src/appl/telnet/telnetd/configure.in	2002-11-15 21:21:51.000000000 +0100
++++ krb5-1.3.3/src/appl/telnet/telnetd/configure.in	2004-07-29 16:42:02.000000000 +0200
+@@ -22,7 +22,7 @@
+ fi
+ fi
+ AC_HEADER_TIME
+-AC_CHECK_HEADERS(string.h arpa/nameser.h utmp.h sys/time.h sys/tty.h sac.h sys/ptyvar.h sys/filio.h sys/stream.h sys/utsname.h memory.h)
++AC_CHECK_HEADERS(string.h arpa/nameser.h utmp.h sys/time.h sys/tty.h sys/ttycom.h sac.h sys/ptyvar.h sys/filio.h sys/stream.h sys/utsname.h memory.h)
+ AC_CHECK_FUNCS(gettosbyname vsnprintf)
+ KRB5_AC_INET6
+ dnl
+diff -ur krb5-1.3.3.old/src/appl/telnet/telnetd/telnetd.c krb5-1.3.3/src/appl/telnet/telnetd/telnetd.c
+--- krb5-1.3.3.old/src/appl/telnet/telnetd/telnetd.c	2003-01-10 01:14:14.000000000 +0100
++++ krb5-1.3.3/src/appl/telnet/telnetd/telnetd.c	2004-07-29 16:42:02.000000000 +0200
+@@ -59,6 +59,9 @@
+ #include <stdlib.h>
+ #include <libpty.h>
+ #include <com_err.h>
++#ifdef HAVE_SYS_TTYCOM_H
++#include <sys/ttycom.h>
++#endif
+ #if	defined(_SC_CRAY_SECURE_SYS)
+ #include <sys/sysv.h>
+ #include <sys/secdev.h>
+@@ -97,10 +100,15 @@
+ 
+ int	registerd_host_only = 0;
+ 
++#ifdef USE_TERMIO
++# include <termios.h>
++#else
++# include <termio.h>
++#endif
++
+ #ifdef	STREAMSPTY
+ #include <sys/stream.h>
+ # include <stropts.h>
+-# include <termio.h>
+ /* make sure we don't get the bsd version */
+ #ifdef HAVE_SYS_TTY_H
+ # include "/usr/include/sys/tty.h"
+diff -ur krb5-1.3.3.old/src/appl/telnet/telnetd/termstat.c krb5-1.3.3/src/appl/telnet/telnetd/termstat.c
+--- krb5-1.3.3.old/src/appl/telnet/telnetd/termstat.c	2001-08-03 00:07:00.000000000 +0200
++++ krb5-1.3.3/src/appl/telnet/telnetd/termstat.c	2004-07-29 16:44:07.000000000 +0200
+@@ -33,6 +33,7 @@
+ 
+ /* based on @(#)termstat.c	8.1 (Berkeley) 6/4/93 */
+ 
++#include <sys/ioctl.h> /* TIOCSWINSZ */
+ #include "telnetd.h"
+ 
+ /*
+diff -ur krb5-1.3.3.old/src/config/shlib.conf krb5-1.3.3/src/config/shlib.conf
+--- krb5-1.3.3.old/src/config/shlib.conf	2004-07-29 17:24:54.000000000 +0200
++++ krb5-1.3.3/src/config/shlib.conf	2004-07-29 17:26:18.000000000 +0200
+@@ -277,7 +277,7 @@
+ 	CC_LINK_STATIC='LD_LIBRARY_PATH=`echo $(PROG_LIBPATH) | sed -e "s/-L//g" -e "s/ /:/g` $(PURE) $(CC) $(CFLAGS) $(LDFLAGS)'
+ 	RUN_ENV='LD_LIBRARY_PATH=`echo $(PROG_LIBPATH) | sed -e "s/-L//g" -e "s/ /:/g"`; export LD_LIBRARY_PATH;'
+ 	;;
+-*-*-linux*)
++*-*-linux* | *-*-gnu* | *-*-k*bsd*-gnu)
+ 	PICFLAGS=-fPIC
+ 	SHLIBVEXT='.so.$(LIBMAJOR).$(LIBMINOR)'
+ 	SHLIBSEXT='.so.$(LIBMAJOR)'
