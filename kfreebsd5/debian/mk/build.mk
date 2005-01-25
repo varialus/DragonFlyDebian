7fef6ec9a00e51af981104b8b44eb135ea8c347f
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2005-01-25 12:14:02 +0000

    * Add RTC (real time clock) driver implementation.   - rtc.tar.gz:  New.   - rules:  Build rtc.

--- kfreebsd5/debian/mk/build.mk
+++ kfreebsd5/debian/mk/build.mk
@@ -1,14 +1,24 @@
+full_version	:= $(shell dpkg-parsechangelog | grep ^Version | sed -e 's/^.*: //g')
+version		:= $(shell echo $(full_version) | sed -e 's/[+-].*//g')
+major		:= $(shell echo $(version) | sed -e 's/\..*//g')
+revision	:= $(shell echo $(full_version) | sed -e 's/^[^+-]*//g')
+
 KERNEL		:= GENERIC
 PATH		:= /usr/lib/freebsd:$(CURDIR)/bin:$(PATH)
 DESTDIR		:= $(CURDIR)/debian/kfreebsd$(major)
 MAKE		:= make DESTDIR=$(DESTDIR) WERROR=
-revision	:= `dpkg-parsechangelog | grep ^Version | cut -c 10- | cut -d '-' -f 2`
+
 kfreebsd_cpu	:= $(DEB_HOST_GNU_CPU)
 ifeq ($(kfreebsd_cpu), x86_64)
 kfreebsd_cpu	:= amd64
 endif
 
-build/kfreebsd$(major):: debian/stamp-build
+package		:= kfreebsd$(major)
+export SRC_BASE=$(CURDIR)/$(DEB_SRCDIR)
+export SYS=$(SRC_BASE)/sys
+export SYSDIR=$(SYS)
+
+build/$(package):: debian/stamp-build
 debian/stamp-build: apply-patches
 	# make is stupid
 	if ! test -e debian/stamp-build ; then \
@@ -19,10 +29,12 @@ debian/stamp-build: apply-patches
 		&& config $(KERNEL) ; \
 	cd $(CURDIR)/$(DEB_SRCDIR)/sys/$(kfreebsd_cpu)/compile/$(KERNEL)/ \
 		&& $(MAKE) depend && $(MAKE) ; \
+	cd $(CURDIR)/$(DEB_SRCDIR)/../rtc/ \
+		&& $(MAKE) ; \
 	touch $(CURDIR)/debian/stamp-build ; \
 	fi
 
-binary/kfreebsd$(major):: common-install-prehook-arch
+binary/$(package):: common-install-prehook-arch
 	# setup directories to install into first
 	for i in $(kfreebsd_MKDIR); do \
 		install -d -m 755 -o root -g root $(DESTDIR)/$$i; \
