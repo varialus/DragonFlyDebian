2976bb0558fc2ea0b47355e7c35cc2ec6a70f8d2
aurel32 <aurel32@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2005-09-17 03:49:45 +0000

    remove mpd patch (NMUed)

--- web/patches/libao.bash
+++ web/patches/libao.bash
@@ -2,20 +2,18 @@
 
 # Status: in BTS.
 
-cp debian/control{,.in}
 cat $0 | patch -p1
-fakeroot debian/rules clean
 exit 0
 
-diff -ur libao-0.8.5.old/debian/control.in libao-0.8.5/debian/control.in
---- libao-0.8.5.old/debian/control.in	2004-09-19 17:28:59.000000000 +0200
-+++ libao-0.8.5/debian/control.in	2004-09-19 17:30:11.000000000 +0200
+diff -ur libao-0.8.5.old/debian/control libao-0.8.5/debian/control
+--- libao-0.8.5.old/debian/control	2004-09-19 17:28:59.000000000 +0200
++++ libao-0.8.5/debian/control	2004-09-19 17:30:11.000000000 +0200
 @@ -2,7 +2,7 @@
  Section: libs
  Priority: optional
  Maintainer: Christopher L Cheney <ccheney@debian.org>
 -Build-Depends: autotools-dev, debhelper (>> 4.0.18), devscripts, gawk, libartsc0-dev, libasound2-dev, libaudio-dev, libesd0-dev, libxt-dev, xlibs-static-dev
-+Build-Depends: autotools-dev, debhelper (>> 4.0.18), devscripts, gawk, libartsc0-dev, libasound2-dev [@linux-gnu@], libaudio-dev, libesd0-dev, libxt-dev, xlibs-static-dev, type-handling (>= 0.2.1)
++Build-Depends: autotools-dev, debhelper (>> 4.0.18), devscripts, gawk, libartsc0-dev, libasound2-dev [!kfreebsd-i386], libaudio-dev, libesd0-dev, libxt-dev, xlibs-static-dev
  Standards-Version: 3.6.1.0
  
  Package: libao2
@@ -53,17 +51,3 @@ diff -ur libao-0.8.5.old/debian/libao2.install libao-0.8.5/debian/libao2.install
 -debian/tmp/usr/lib/ao/plugins-2/liboss.so
 +debian/tmp/usr/lib/ao/plugins-2/lib*.so
  debian/tmp/usr/lib/libao.so.*
-diff -ur libao-0.8.5.old/debian/rules libao-0.8.5/debian/rules
---- libao-0.8.5.old/debian/rules	2004-03-17 06:56:48.000000000 +0100
-+++ libao-0.8.5/debian/rules	2004-09-19 17:32:12.000000000 +0200
-@@ -89,6 +89,10 @@
- 		$(MAKE) cvs-clean ;\
- 	fi
- 
-+	cat debian/control.in \
-+	| sed "s/@linux-gnu@/`type-handling any linux-gnu`/g" \
-+	> debian/control
-+
- 	dh_clean
- 
- install: install-indep install-arch
