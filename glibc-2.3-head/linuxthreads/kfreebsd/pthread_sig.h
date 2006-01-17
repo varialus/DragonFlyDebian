9ca50d33d4230e6035c925e91c2831b01a335a84
ps-guest <ps-guest@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-01-17 12:50:41 +0000

    remove remaining ugly-hacks

---
+++ glibc-2.3-head/linuxthreads/kfreebsd/pthread_sig.h
@@ -0,0 +1,6 @@
+/* Signal numbers used for the communication. */
+                                          
+int __pthread_sig_restart = 32;
+int __pthread_sig_cancel  = 33;
+int __pthread_sig_debug   = 34;
+
