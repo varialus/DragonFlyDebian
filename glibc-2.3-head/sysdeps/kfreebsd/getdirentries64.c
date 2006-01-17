10c63bf118fe0668906f78d7f99bf9d45c8f2e05
aurel32 <aurel32@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-01-17 15:22:59 +0000

    Move the sysdeps/ to glibc-ports-2.3.5/

--- glibc-2.3-head/sysdeps/kfreebsd/getdirentries64.c
+++ glibc-2.3-head/sysdeps/kfreebsd/getdirentries64.c
@@ -2,4 +2,4 @@
    and 'getdirentries' differ only in the type of the BASEP argument.  */
 #define GETDIRENTRIES getdirentries64
 #define OFF_T off64_t
-#include <sysdeps/unix/bsd/bsd4.4/kfreebsd/getdirentries.c>
+#include <getdirentries.c>
