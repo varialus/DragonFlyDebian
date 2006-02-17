64a5d859c63dbe2eab916e5897cef20cbc7c4ce3
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-02-17 18:15:42 +0000

    * Rename to kfreebsd-common.   * init.d: New. Fix /dev permissions on startup.

---
+++ kfreebsd-common/debian/init.d
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# skeleton	example file to build /etc/init.d/ scripts.
+#		This file should be used to construct scripts for /etc/init.d.
+#
+#		Written by Miquel van Smoorenburg <miquels@cistron.nl>.
+#		Modified for Debian 
+#		by Ian Murdock <imurdock@gnu.ai.mit.edu>.
+#
+# Version:	@(#)skeleton  1.9  26-Feb-2001  miquels@cistron.nl
+#
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+set -e
+
+echo -n "Setting up /dev permissions..."
+
+# only do this during boot, to avoid messing up ttys
+if [ "$RUNLEVEL" = "S" ] ; then
+  chown root:root /dev/*
+fi
+
+for i in /dev/dsp{,[0-9]} /dev/mixer{,[0-9]} /dev/audio{,ctl,[0-9]} ; do
+  if test -e $i ; then
+    chgrp audio $i
+  fi
+done
+for i in /dev/{,a}cd[0-9] ; do
+  if test -e $i ; then
+    chgrp cdrom $i
+  fi
+done
+for i in /dev/console /dev/ptyp[0-9] ; do
+  if test -e $i ; then
+    chgrp tty $i
+  fi
+done
+for i in /dev/fd[0-9] ; do
+  if test -e $i ; then
+    chgrp floppy $i
+  fi
+done
+for i in /dev/ad[0-9]* ; do
+  if test -e $i ; then
+    chgrp disk $i
+  fi
+done
+for i in /dev/{,k}mem ; do
+  if test -e $i ; then
+    chgrp kmem $i
+  fi
+done
+for i in /dev/lpt[0-9]* ; do
+  if test -e $i ; then
+    chgrp lp $i
+  fi
+done
+for i in /dev/cuaa[0-9] ; do
+  if test -e $i ; then
+    chgrp dialout $i
+  fi
+done
+
+echo "done."
+
+exit 0
