94d8976377bac60b05ebb3052a1198a07e0aa777
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-03-19 10:50:43 +0000

    * Integrate kldutils package into freebsd-utils. * fetch: Obtain source for kldutils and net-tools.   * Stop linking with libedit-dev (vis/unvis functions are in libbsd now).   * Add  tag to freebsd-utils.   * Remove syscall() hacks (will use proper glibc now).

---
+++ freebsd-utils/debian/kldutils.init.d
@@ -0,0 +1,31 @@
+#! /bin/sh
+#
+# skeleton	example file to build /etc/init.d/ scripts.
+#		This file should be used to construct scripts for /etc/init.d.
+#
+#		Written by Miquel van Smoorenburg <miquels@cistron.nl>.
+#		Modified for Debian 
+#		by Ian Murdock <imurdock@gnu.ai.mit.edu>.
+#
+# Version:	@(#)skeleton  1.9  26-Feb-2001  miquels@cistron.nl
+#
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+for i in load stat unload ; do
+  test -x /sbin/kld$i || exit 1
+done
+modules="`shopt -s nullglob ; cat /etc/modules /etc/modules.d/* \
+  | sed -e \"s/#.*//g\" -e \"/^\( \|\t\)*$/d\" | sort | uniq`"
+
+set -e
+
+for i in ${modules} ; do
+  if ! kldstat -n $i >/dev/null 2>/dev/null ; then
+    echo "Loading $i ..."
+    kldload $i || true
+  else
+    echo "Not loading $i (already loaded)"
+  fi
+done
+
+exit 0
