ef225ce7936314131f6d740fd90a97e55ba45b0a
ps-guest <ps-guest@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-06-12 12:19:42 +0000

    * glibc 2.4 fixes

--- glibc-2.3-head/sysdeps/kfreebsd/bits/sched.h
+++ glibc-2.3-head/sysdeps/kfreebsd/bits/sched.h
@@ -44,10 +44,10 @@ struct sched_param
 # define CLONE_FS      0x00000200 /* Set if fs info shared between processes.  */
 # define CLONE_FILES   0x00000400 /* Set if open files shared between processes.  */
 # define CLONE_SIGHAND 0x00000800 /* Set if signal handlers shared.  */
-# define CLONE_PID     0x00001000 /* Set if pid shared.  */
 # define CLONE_PTRACE  0x00002000 /* Set if tracing continues on the child.  */
 # define CLONE_VFORK   0x00004000 /* Set if the parent wants the child to
 				     wake it up on mm_release.  */
+# define CLONE_SYSVSEM 0x00040000 /* share system V SEM_UNDO semantics */
 #endif
 
 __BEGIN_DECLS
