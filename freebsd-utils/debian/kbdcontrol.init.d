4b8fcc025818275d44d93d710f6417ce0a3ed009
aurel32 <aurel32@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-11-07 15:05:06 +0000

    * Add start/stop options to /etc/init.d/kbdcontrol   * Add LSB section to /etc/init.d/kbdcontrol   * Use debhelper code to call /etc/init.d/kbdcontrol

--- freebsd-utils/debian/kbdcontrol.init.d
+++ freebsd-utils/debian/kbdcontrol.init.d
@@ -1,4 +1,13 @@
 #! /bin/sh
+### BEGIN INIT INFO
+# Provides:             keymap
+# Required-Start:       
+# Required-Stop:
+# Default-Start:        S
+# Default-Stop:
+# Short-Description:    Set keymap
+# Description:          Set the Console keymap
+### END INIT INFO
 #
 # skeleton	example file to build /etc/init.d/ scripts.
 #		This file should be used to construct scripts for /etc/init.d.
@@ -15,10 +24,29 @@ set -e
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 which kbdcontrol >/dev/null
 
-if test -e /etc/kbdcontrol.conf ; then
-  echo -n "Loading console keymap..."
-  kbdcontrol -l `grep -v ^# /etc/kbdcontrol.conf` < /dev/console
-  echo "done."
-fi
+do_start() {
+	if test -e /etc/kbdcontrol.conf ; then
+		echo -n "Loading console keymap..."
+		kbdcontrol -l `grep -v ^# /etc/kbdcontrol.conf` < /dev/console
+		echo "done."
+	fi
+}
+
+case "$1" in
+  start|"")
+	do_start
+	;;
+  restart|reload|force-reload)
+	echo "Error: argument '$1' not supported" >&2
+	exit 3
+	;;
+  stop)
+	# No-op
+	;;
+  *)
+	echo "Usage: mountall.sh [start|stop]" >&2
+	exit 3
+	;;
+esac
 
 exit 0
