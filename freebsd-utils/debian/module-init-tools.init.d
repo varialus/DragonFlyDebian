d69ecd7e6d318058b280b591b76e90d554179926
aurel32 <aurel32@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-11-07 14:37:07 +0000

    [ Aurelien Jarno ]   * Bumped Standards-Version to 3.7.2 (no changes).   * Add LSB section to /etc/init.d/module-init-tools   * Add start/stop options to /etc/init.d/module-init-tools   * Drop/adjust dependencies on non-official version of the glibc.

--- freebsd-utils/debian/module-init-tools.init.d
+++ freebsd-utils/debian/module-init-tools.init.d
@@ -1,4 +1,15 @@
 #! /bin/sh
+### BEGIN INIT INFO
+# Provides:          module-init-tools
+# Required-Start:
+# Required-Stop:
+# Should-Start:      checkroot
+# Should-stop:
+# Default-Start:     S
+# Default-Stop:
+# Short-Description: Process /etc/modules.
+# Description:       Load the modules listed in /etc/modules.
+### END INIT INFO
 #
 # skeleton	example file to build /etc/init.d/ scripts.
 #		This file should be used to construct scripts for /etc/init.d.
@@ -11,22 +22,42 @@
 #
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-for i in load stat unload ; do
-  which kld$i >/dev/null || exit 1
-done
-modules="`shopt -s nullglob ; cat /etc/modules /etc/modules.d/* \
-  | sed -e \"s/#.*//g\" -e \"/^\( \|\t\)*$/d\" | sort | uniq`"
 
-set -e
+do_start() {
+	for i in load stat unload ; do
+		which kld$i >/dev/null || exit 1
+	done
+	modules="`shopt -s nullglob ; cat /etc/modules /etc/modules.d/* \
+		 | sed -e \"s/#.*//g\" -e \"/^\( \|\t\)*$/d\" | sort | uniq`"
 
-for i in ${modules} ; do
-  if ! kldstat -n $i >/dev/null 2>/dev/null ; then
-    echo "Loading $i ..."
-    kldload $i || true
-    echo "... done."
-  else
-    echo "Not loading $i (already loaded)"
-  fi
-done
+	set -e
+
+	for i in ${modules} ; do
+		if ! kldstat -n $i >/dev/null 2>/dev/null ; then
+			echo "Loading $i ..."
+			kldload $i || true
+			echo "... done."
+		else
+			echo "Not loading $i (already loaded)"
+		fi
+	done
+}
+
+case "$1" in
+  start|"")
+	do_start
+	;;
+  restart|reload|force-reload)
+	echo "Error: argument '$1' not supported" >&2
+	exit 3
+	;;
+  stop)
+	# No-op
+	;;
+  *)
+	echo "Usage: mountall.sh [start|stop]" >&2
+	exit 3
+	;;
+esac
 
 exit 0
