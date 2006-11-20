cd7d597824bcba34f0ef0fce1eb2c313b1e3eb8b
ps-guest <ps-guest@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2006-11-20 09:00:18 +0000

    * move <sys/ktrace.h> from libc0.1-dev into kfreebsd-kernel-headers   this way it gets also updated

--- glibc-2.3-head/sysdeps/kfreebsd/bits/uio.h
+++ glibc-2.3-head/sysdeps/kfreebsd/bits/uio.h
@@ -35,3 +35,15 @@ struct iovec
    writev() system call.  For larger arrays of 'struct iovec', the libc
    uses a single read() or write() call to guarantee atomicity.  */
 #define UIO_MAXIOV	1024
+
+#ifdef __USE_BSD
+enum    uio_rw { UIO_READ, UIO_WRITE };
+
+/* Segment flag values. */
+enum uio_seg {
+        UIO_USERSPACE,          /* from user data space */
+        UIO_SYSSPACE,           /* from system space */
+        UIO_NOCOPY              /* don't copy, already in object */
+};
+#endif
+
