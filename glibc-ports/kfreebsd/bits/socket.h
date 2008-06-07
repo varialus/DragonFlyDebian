a3961eb53f2118eec3318d4362b1bfe28a8e7a2c
ps-guest <ps-guest@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2008-06-07 15:53:30 +0000

    * #define LOCAL_PEERCRED, LOCAL_CREDS, LOCAL_CONNWAIT in <bits/socket.h>   see http://lists.debian.org/debian-bsd/2008/06/msg00001.html

--- glibc-ports/kfreebsd/bits/socket.h
+++ glibc-ports/kfreebsd/bits/socket.h
@@ -355,6 +355,11 @@ enum
 #define SO_TYPE SO_TYPE
   };
 
+/* Socket options. */
+#define LOCAL_PEERCRED          0x001   /* retrieve peer credentials */
+#define LOCAL_CREDS             0x002   /* pass credentials to receiver */
+#define LOCAL_CONNWAIT          0x004   /* connects block until accepted */
+
 /* Structure used to manipulate the SO_LINGER option.  */
 struct linger
   {
