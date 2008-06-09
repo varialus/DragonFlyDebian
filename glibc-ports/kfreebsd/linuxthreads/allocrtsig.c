e088ce5d96863f6ee82deb6a3f071dd458c89b34
ps-guest <ps-guest@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2008-06-09 16:11:21 +0000

    * use RT behaviour of signals, when available   perl "ext/threads/shared/t/stress" passes with it under FreeBSD 7.x

--- glibc-ports/kfreebsd/linuxthreads/allocrtsig.c
+++ glibc-ports/kfreebsd/linuxthreads/allocrtsig.c
@@ -1,4 +1,14 @@
+#if 1
+
+#define __SIGRTMIN 65
+#define __SIGRTMAX 126
+#include <linuxthreads/sysdeps/unix/sysv/linux/allocrtsig.c>
+
+#else
+
 #include <signal/allocrtsig.c>
 strong_alias (__libc_current_sigrtmin, __libc_current_sigrtmin_private);
 strong_alias (__libc_current_sigrtmax, __libc_current_sigrtmax_private);
 strong_alias (__libc_allocate_rtsig, __libc_allocate_rtsig_private);
+
+#endif
---
+++ glibc-ports/kfreebsd/testrtsig.h
@@ -0,0 +1,44 @@
+/* Test whether RT signals are really available.
+   Copyright (C) 2008 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include <sys/sysctl.h>
+#include <errno.h>
+#include <string.h>
+
+#include <kernel-features.h>
+
+static int
+kernel_has_rtsig (void)
+{
+#if __ASSUME_REALTIME_SIGNALS
+  return 1;
+#else
+
+  int request[2] = { CTL_KERN, KERN_OSRELDATE};
+  size_t len;
+  int val;
+
+  len = sizeof (val);
+  if (__sysctl (request, 2, &val, &len, NULL, 0) < 0)
+      return 0;
+  if ( val < 700050) /* FreeBSD 7.0 is 700055 */
+      return 0;
+  return 1;    
+#endif
+}
