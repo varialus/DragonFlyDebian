dba720f6e9df5fd9ae45a068e5f242c24427f188
ps-guest <ps-guest@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2009-02-19 08:18:55 +0000

    * integrate glibc-2.8+ specific sysdeps

--- glibc-ports/gen_sysdep_patch.sh
+++ glibc-ports/gen_sysdep_patch.sh
@@ -4,8 +4,12 @@ LANG=C
 
 # sysdeps dir
 tmp=`mktemp -d`
-mkdir -p ${tmp}/ports/sysdeps/unix/bsd/bsd4.4
-cp -a kfreebsd ${tmp}/ports/sysdeps/unix/bsd/bsd4.4/
-(cd ${tmp} && diff -x .svn -Nurd null ports/ ) > sysdeps.diff
+mkdir -p ${tmp}/b/ports/sysdeps/unix/bsd/bsd4.4
+cp -a kfreebsd ${tmp}/b/ports/sysdeps/unix/bsd/bsd4.4/
+(cd ${tmp} && diff -x .svn -Nurd null b/ports ) | \
+   sed -e "sz^--- null/sysdeps/unix/bsd/bsd4.4/kfreebsd/.*z--- /dev/nullz" \
+       -e "\z^diff -x .svn -Nurd null/sysdeps/unix/bsd/bsd4.4/kfreebsd/.*zd" \
+       -e "\z^+++ b/ports/sysdeps/unix/bsd/bsd4.4/kfreebsd/zsz\t20[0-9][0-9].*zz" \
+   > sysdeps.diff
 rm -rf ${tmp}
 
