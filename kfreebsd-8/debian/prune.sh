1101179bd1b4175054ddab3b2674c4dccb7f6d89
ps-guest <ps-guest@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2010-11-16 11:48:00 +0000

    drop non-free firmware, builds fine on ka, it needs to be tested on ki raise NKPT on amd64 re-enable ulpt

---
+++ kfreebsd-8/debian/prune.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+rm -vf `find sys -name *.uu`
+rm -vf sys/gnu/dev/sound/pci/csaimg.h
+rm -vf sys/gnu/dev/sound/pci/maestro3_dsp.h 
+rm -vf sys/dev/drm/mga_ucode.h
+rm -vf sys/dev/drm/r128_cce.c
+rm -vf sys/dev/drm/r600_microcode.h
+rm -vf sys/dev/drm/radeon_microcode.h
+rm -vf sys/dev/txp/3c990img.h
+rm -vf sys/dev/fxp/rcvbundl.h
+rm -vf sys/dev/digi/*X*.h
+rm -vf sys/dev/sf/starfire_rx.h
+rm -vf sys/dev/sf/starfire_tx.h
+rm -vf sys/dev/sn/ositech.h
+rm -vf sys/dev/sound/pci/ds1-vfw.h
+rm -vf sys/dev/si/si2_z280.c
+rm -vf sys/dev/si/si3_t225.c
+rm -vf sys/dev/cxgb/common/cxgb_ael1002.c
+rm -vf sys/dev/fatm/firmware.h
+rm -vf sys/dev/cx/csigmafw.h
+rm -vf sys/dev/bce/if_bcefw.h
+rm -vf sys/dev/usb/net/if_kuefw.h
+rm -vf sys/dev/usb/wlan/if_rumfw.h
+rm -vf sys/dev/usb/wlan/if_zydfw.h
+rm -vf sys/dev/ti/ti_fw2.h
+rm -vf sys/dev/ti/ti_fw.h
+rm -vf sys/dev/ctau/ctaue1fw.h
+rm -vf sys/dev/ctau/ctau2fw.h
+rm -vf sys/dev/ctau/ctaufw.h
+rm -vf sys/dev/ctau/ctaug7fw.h
+rm -vf sys/dev/ispfw/asm_*.h
+rm -vf sys/dev/advansys/adwmcode.c
+rm -vf sys/dev/ce/tau32-ddk.c
+rm -vf sys/dev/cp/cpddk.c
