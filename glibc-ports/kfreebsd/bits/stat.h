98e209b2c94f05a4c4474fb80a906f3aaa18cf7a
aurel32 <aurel32@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2011-02-08 16:32:12 +0000

    Allow inclusion of <bits/stat.h> from <fcntl.h> similarly to what has been done in upstream commit f095bb7204d80f609a73a22796edd6cffd4c6add

--- glibc-ports/kfreebsd/bits/stat.h
+++ glibc-ports/kfreebsd/bits/stat.h
@@ -16,7 +16,7 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
-#ifndef _SYS_STAT_H
+#if !defined _SYS_STAT_H && !defined _FCNTL_H
 # error "Never include <bits/stat.h> directly; use <sys/stat.h> instead."
 #endif
 
