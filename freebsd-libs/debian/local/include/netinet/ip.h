cf719a9bf06bcb760f098651564e5925f61da8ad
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2012-01-07 13:24:31 +0000

    Fix undefined reference to ip_next. Thanks Christoph Egger. (Closes: #654581)

---
+++ freebsd-libs/debian/local/include/netinet/ip.h
@@ -0,0 +1,7 @@
+/* BSD-style include guards (used by e.g. alias_local.h) */
+#ifndef _NETINET_IP_H_
+#define _NETINET_IP_H_
+
+#include_next <netinet/ip.h>
+
+#endif
--- freebsd-libs/debian/local/include/netinet/udp.h
+++ freebsd-libs/debian/local/include/netinet/udp.h
@@ -1,3 +1,7 @@
+/* BSD-style include guards (used by e.g. alias_local.h) */
+#ifndef _NETINET_UDP_H_
+#define _NETINET_UDP_H_
+
 #include <features.h>
 #if __FAVOR_BSD
 #  include_next <netinet/udp.h>
@@ -5,4 +9,6 @@
 #  define __FAVOR_BSD 1
 #  include_next <netinet/udp.h>
 #  undef __FAVOR_BSD
+#endif
+
 #endif
