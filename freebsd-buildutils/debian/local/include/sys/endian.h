2c1e5e03151b6db9418ba1355d84965146132b68
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2012-01-28 18:25:07 +0000

    Fix local endian.h to use native <sys/endian.h> on GNU/kFreeBSD.

--- freebsd-buildutils/debian/local/include/sys/endian.h
+++ freebsd-buildutils/debian/local/include/sys/endian.h
@@ -1,4 +1,9 @@
-#ifndef _SYS_ENDIAN_H_
-#define _SYS_ENDIAN_H_
-#include_next <machine/endian.h>
+#ifdef __FreeBSD_kernel__
+/* We have <sys/endian.h>.  Use it.  */
+# include_next <sys/endian.h>
+#else
+# ifndef _SYS_ENDIAN_H_
+#  define _SYS_ENDIAN_H_
+#  include_next <machine/endian.h>
+# endif
 #endif
