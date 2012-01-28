257e9c0299da8f019de2b9e36ebde5b680946263
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2012-01-28 18:24:01 +0000

    Add __BEGIN_DECLS / __END_DECLS in local unistd.h.

--- freebsd-buildutils/debian/local/include/unistd.h
+++ freebsd-buildutils/debian/local/include/unistd.h
@@ -3,7 +3,12 @@
 #include_next <unistd.h>
 
 #define getopt(argc, argv, options) bsd_getopt(argc, argv, options)
+
+__BEGIN_DECLS
+
 int bsd_getopt(int, char **, char *);
 extern int optreset;
+
+__END_DECLS
 
 #endif
