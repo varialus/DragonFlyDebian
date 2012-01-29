60bddcf88553e2ea03322f27a5922c22d88302f9
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2012-01-29 00:55:11 +0000

    Emulate implicit includes on FreeBSD

--- freebsd-buildutils/debian/local/include/sys/types.h
+++ freebsd-buildutils/debian/local/include/sys/types.h
@@ -2,6 +2,12 @@
 #define	_SYS_TYPES_H_
 #include_next <sys/types.h>
 #include <sys/cdefs.h>
-#include <machine/endian.h>
 #include <stdint.h> /* uintXX_t */
+
+#ifdef __FreeBSD_kernel__
+/* Emulate implicit includes on FreeBSD */
+#include <machine/endian.h>
+#include <sys/_types.h>
+#endif
+
 #endif
