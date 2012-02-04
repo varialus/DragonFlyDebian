b29688cfb58e5e0e0364302515af53ed75c14619
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2012-02-04 13:10:13 +0000

    Add implicit includes to sys/stdint.h.

--- freebsd-buildutils/debian/local/include/sys/stdint.h
+++ freebsd-buildutils/debian/local/include/sys/stdint.h
@@ -1,1 +1,14 @@
 #include_next <stdint.h>
+
+#ifndef _SYS_STDINT_H_
+#define _SYS_STDINT_H_
+
+#include <sys/cdefs.h>
+
+#ifdef __FreeBSD_kernel__
+#include <sys/_types.h>
+#include <machine/_stdint.h>
+#include <sys/_stdint.h>
+#endif
+
+#endif
