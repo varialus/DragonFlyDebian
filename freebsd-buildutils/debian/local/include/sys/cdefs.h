021a283110ba20ddf27e2c66242858f324b30b0e
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2012-02-04 13:16:32 +0000

    Steal a few more definitions for sys/cdefs.h.

--- freebsd-buildutils/debian/local/include/sys/cdefs.h
+++ freebsd-buildutils/debian/local/include/sys/cdefs.h
@@ -38,8 +38,14 @@
 
 #include_next <sys/cdefs.h>
 
-#define __dead2		__attribute__((__noreturn__))
-#define __unused	__attribute__((__unused__))
+#define	__dead2		__attribute__((__noreturn__))
+#define	__pure2		__attribute__((__const__))
+#define	__unused	__attribute__((__unused__))
+#define	__used		__attribute__((__used__))
+#define	__packed	__attribute__((__packed__))
+#define	__aligned(x)	__attribute__((__aligned__(x)))
+#define	__section(x)	__attribute__((__section__(x)))
+
 #define __offsetof(type, field)	__builtin_offsetof(type, field)
 #define __printflike(fmtarg, firstvararg) \
 	__attribute__((__format__ (__printf__, fmtarg, firstvararg)))
