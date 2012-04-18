9135b9f21cbddf6e11fee76ad2c4cdb1e2d4de79
rmh <rmh@ae44cbe4-c7d5-0310-ae45-95c72a56cd7d>
2012-04-18 21:15:37 +0000

    Do not remove out[bwl] from <machine/cpufunc.h>, rename them instead (and rename their users) to avoid conflict with glibc <sys/io.h>.  This will help un-breaking <machine/bus.h>.

--- kfreebsd-kernel-headers/test/cpufunc.c
+++ kfreebsd-kernel-headers/test/cpufunc.c
@@ -1,7 +1,4 @@
 #include <sys/types.h>
 
-/* Include potentially conflicting declarations: inb(), inw(), outb(), etc. */
-#include <sys/io.h>
-
 /* Include <machine/cpufunc.h> WITHOUT explicit <stdint.h>. */
 #include <machine/cpufunc.h>
